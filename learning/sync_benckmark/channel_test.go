dbce9c67cc933e5e2dfca3114bbf9d9d4b97f72a
Ryoma <1179801728@qq.com>
2022-06-15 12:06:43 +0800

    feat: channel and goruntine test

---
+++ learning/sync_benckmark/channel_test.go
@@ -0,0 +1,43 @@
+package sync_benckmark
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsyncService() chan string {
+	retCh := make(chan string)
+	// 注意 Channel 的类型
+	//retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		// 注意此条记录输出的时间
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	retCh := AsyncService()
+	otherTask()
+	fmt.Println(<-retCh)
+}
